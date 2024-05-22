7f5c41633d34a4732cabd5e8a6c57e6b83dc548a
Abel Lavieri <abel.lavieri@gmail.com>
2024-05-22 02:57:07 -0400

    Modify part1/unicafe/App.jsx buttons to be their own components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -42,8 +42,9 @@ const Statistics = ({ good, neutral, bad }) => {
   )
 }
 
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -52,9 +53,9 @@ const App = () => {
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
       </div>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
